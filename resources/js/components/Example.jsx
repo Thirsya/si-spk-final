cf4093c4f21778c3db46b0423bb9cf8e9c1c16b5
Thirsya <thirsya.widya@gmail.com>
2023-05-28 21:47:02 +0700

    first commit

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Example() {
+  return (
+    <div>
+      <h1>Hello, React Raka Rafif Thirsya</h1>
+      <h1>Hello, React Raka Rafif Thirsya</h1>
+      <h1>Hello, React Raka Rafif Thirsya</h1>
+    </div>
+  );
+}
+
+export default Example;
